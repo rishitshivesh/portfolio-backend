9d9e8861ead0a69e950a919feab0879c0d36ebed
Rishit Shivesh <rishitshivesh@gmail.com>
2023-03-09 00:15:48 +0530

    blogs

---
+++ routes/blogs.routes.js
@@ -0,0 +1,80 @@
+const Router = require("express").Router;
+const router = Router();
+const uuid = require("uuid");
+// const db = require("../db/index").dbClient;
+const db = require("../utils/Dynamo");
+const { isAuthenticated } = require("../middlewares/auth.middleware");
+const TableName = process.env.TABLE_NAME;
+const UserName = process.env.USER_NAME;
+require("dotenv").config();
+
+router.get("/", isAuthenticated, async (req, res) => {
+  try {
+    const blogs = await db.queryBeginsWith(UserName, "blogs", TableName);
+    res.json({ data: blogs, status: 200 });
+  } catch (err) {
+    res.json({ data: err, status: 500 });
+  }
+  //   res.send("Hello World! blogs are here");
+});
+
+router.post("/", isAuthenticated, async (req, res) => {
+  const { title, date, author, body, raw } = req.body;
+  // console.log(req.body.year);
+  if (!title || !date || !author || !body || !raw) {
+    return res.json({ data: "Please fill all the fields", status: 422 });
+  }
+  const id = uuid.v4();
+  const created = new Date().toISOString();
+  // const check_if_exists
+  // const query =
+  //   "INSERT INTO cassandra.music (id, name, artist, album, year, src, album_art, art, created) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)";
+  try {
+    const blog = {
+      pk: UserName,
+      sk: `blogs#${id}`,
+      id,
+      ...req.body,
+      created,
+    };
+    const data = await db.put(blog, TableName);
+    // console.log(blog);
+    res.json({ data: data, status: 200 });
+  } catch (err) {
+    res.json({ data: err, status: 500 });
+  }
+});
+
+router.post("/batchAdd", isAuthenticated, async (req, res) => {
+  const data = req.body;
+
+  // // batch add blogs
+
+  // const query =
+  //   "INSERT INTO cassandra.music (id, name, artist, album, year, src, album_art, art, created) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)";
+  // //   const params = [];
+  const queries = [];
+  data.forEach((blog) => {
+    const id = uuid.v4();
+    const created = new Date().toISOString();
+    const s = {
+      pk: UserName,
+      sk: `blogs#${id}`,
+      id,
+      ...blog,
+      created,
+    };
+    queries.push(s);
+  });
+  try {
+    // const blogs = await db.batch(queries);
+    queries.forEach(async (blog) => {
+      await db.put(blog, TableName);
+    });
+    const blogs = queries;
+    res.json({ data: blogs, status: 200 });
+  } catch (err) {
+    res.json({ data: err, status: 500 });
+  }
+});
+exports.router = router;
